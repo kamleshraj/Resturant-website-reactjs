ea8b80bf9639ead4494bc90b305a1fc15b404b00
kamleshraj <kamleshk.uiuxdesigner@gmail.com>
2023-03-04 20:07:01 +0530

    removed unwanted component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,13 @@ import { Typography } from '@mui/material';
 const Home = () => {
   return (
     <Layout>
+      <Box>
       <Container fixed>
         <Typography component="h4">
           Main Carousal Header will come here....
         </Typography>
-      </Container> 
+        </Container>
+      </Box>
     </Layout>
   )
 }
