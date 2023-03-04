b4bd358d1c5a7d62f0af0578f7478ad02abbc050
kamleshraj <kamleshk.uiuxdesigner@gmail.com>
2023-03-04 19:51:44 +0530

    place footer in the container

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
+import { Typography } from '@mui/material'
+import { Box, Container } from '@mui/system'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      footer will come here...
-    </div>
+    <Box>
+      <Container>
+        <Typography component='h4'>
+        footer will come here...
+        </Typography>
+      </Container>
+    </Box>
   )
 }
 
