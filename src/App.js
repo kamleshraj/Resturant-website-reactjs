7d46ae2a47cc9fe91b126676c31c9cb8be7fc052
kamleshraj <kamleshk.uiuxdesigner@gmail.com>
2023-03-06 06:17:49 +0530

    use sharedLayout

--- src/App.js
+++ src/App.js
@@ -5,19 +5,38 @@ import Contactus from './pages/Contactus';
 import PageNotFound from './pages/pageNotFound';
 import Menu from './pages/Menu';
 import Home from './pages/Home';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './GlobalStyle';
+import Layout from './components/Layout';
+import Footer from './components/Footer';
 
-const App=()=>{
+const App = () => {
+    const theme = {
+        colors: {
+            heading:'#1e272e',
+            text: '#fff',
+            btnBg: '#0a3d62',
+            borderColor:'rgb(252 188 94)'
+        },
+        responsive:{mobile:'768px',tab:'998px'}
+    }
     return (
-        <>        
-            <BrowserRouter>
+        <>  
+        <ThemeProvider theme={theme}> 
+                <BrowserRouter>
+                    <GlobalStyle/>
                 <Routes>
-                    <Route path='/' element={<Home/>} />
-                    <Route path='/menu' element={<Menu />} />
-                    <Route path='/aboutus' element={<Aboutus />} />
-                    <Route path='/contactus' element={<Contactus />} />
-                    <Route path='*' element={<PageNotFound/>}/>
-                </Routes>
-            </BrowserRouter>
+                    <Route path='/' element={<Layout />}>
+                        <Route index element={<Home />}/>
+                        <Route path='/menu' element={<Menu />} />
+                        <Route path='/aboutus' element={<Aboutus />} />
+                        <Route path='/contactus' element={<Contactus />} />
+                        <Route path='*' element={<PageNotFound />} />
+                    </Route>    
+                    </Routes>
+                    <Footer/>
+                </BrowserRouter>
+        </ThemeProvider>   
         </>
     )
 }
